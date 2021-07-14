dc8b4272995fc879e6bdcb2afe24e712c52aff21
impalakdwivedi <aapalakdwivedi@gmail.com>
2021-07-15 00:02:57 +0530

    react

--- beginnersapproach/src/index.js
+++ beginnersapproach/src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-ReactDOM.render( <
-    div >
-    <
-    h1 > Palak Dwivedi < /h1> <
-    p > Girls are smart < /p> <
-    ol >
-    <
-    li > Studius < /li> <
-    li > funny < /li> <
-    li > caring < /li> <
-    li > honest < /li> <
-    /ol> </div > ,
-    document.getElementById("root")
+const fname = "palak";
+const lname = "dwivedi";
+const currdate= new Date().toLocaleDateString();
+const currtime= new Date().toLocaleTimeString();
+ReactDOM.render( 
+<div> 
+{ /* This is a template literal it helps in not using a 'plus sign' */ } 
+<h1> my name is {lname} </h1> 
+{ /* Another way includes using a hectic method */ } 
+  <br>
+    </br> 
+    <p2> the addition using js in html in jsx is like { 5 + 2 } </p2>
+    <br></br>
+    <p3>the date is {currdate}</p3>
+    <p4>the time is {currtime}</p4>
+</div>
+,document.getElementById("root")
 );
