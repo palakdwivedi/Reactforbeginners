870be76d3dda842f090a4b65b24b98b910b18d38
impalakdwivedi <aapalakdwivedi@gmail.com>
2021-07-14 21:07:44 +0530

    commitagain

--- beginnersapproach/src/index.js
+++ beginnersapproach/src/index.js
@@ -4,6 +4,13 @@ ReactDOM.render( <
     div >
     <
     h1 > Palak Dwivedi < /h1> <
-    /div>,
+    p > Girls are smart < /p> <
+    ol >
+    <
+    li > Studius < /li> <
+    li > funny < /li> <
+    li > caring < /li> <
+    li > honest < /li> <
+    /ol> </div > ,
     document.getElementById("root")
 );
